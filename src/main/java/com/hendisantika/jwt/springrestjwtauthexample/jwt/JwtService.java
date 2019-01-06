da38de23d0b89348275e273d3b91fb75392466dd
Hendi Santika <hendisantika@gmail.com>
2019-01-06 16:54:21 +0700

    #9 - Add JWT Service

---
+++ src/main/java/com/hendisantika/jwt/springrestjwtauthexample/jwt/JwtService.java
@@ -0,0 +1,52 @@
+package com.hendisantika.jwt.springrestjwtauthexample.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static java.time.ZoneOffset.UTC;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-jwt-auth-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-06
+ * Time: 16:53
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtService {
+
+    private static final String ISSUER = "com.chathuranga.examples";
+
+    @Autowired
+    private SecretKeyProvider secretKeyProvider;
+
+    public String generateToken(String username) throws IOException, URISyntaxException {
+        byte[] secretKey = secretKeyProvider.getKey();
+        Date expiration = Date.from(LocalDateTime.now(UTC).plusHours(2).toInstant(UTC));
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(expiration)
+                .setIssuer(ISSUER)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public String verifyToken(String token) throws IOException, URISyntaxException {
+        byte[] secretKey = secretKeyProvider.getKey();
+        Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        //returning authenticated/verified username
+        return claims.getBody().getSubject();
+    }
+}
