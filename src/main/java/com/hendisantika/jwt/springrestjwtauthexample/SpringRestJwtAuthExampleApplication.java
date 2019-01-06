ef1b6d143736cf9f4f1b85981b0c951c15989548
Hendi Santika <hendisantika@gmail.com>
2019-01-06 16:47:01 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/jwt/springrestjwtauthexample/SpringRestJwtAuthExampleApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.jwt.springrestjwtauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJwtAuthExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestJwtAuthExampleApplication.class, args);
+    }
+
+}
+
