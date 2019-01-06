57db5476811b84148e54c9f9a05161862e35fb38
Hendi Santika <hendisantika@gmail.com>
2019-01-07 03:52:53 +0700

    #16 - Add JWT Authentication Entry Point

---
+++ src/main/java/com/hendisantika/jwt/springrestjwtauthexample/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,45 @@
+package com.hendisantika.jwt.springrestjwtauthexample.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-jwt-auth-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-07
+ * Time: 03:52
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+            throws IOException, ServletException {
+        httpServletResponse.setStatus(SC_FORBIDDEN);
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String message;
+        if (e.getCause() != null) {
+            message = e.getCause().getMessage();
+        } else {
+            message = e.getMessage();
+        }
+        byte[] body = new ObjectMapper()
+                .writeValueAsBytes(Collections.singletonMap("error", message));
+        httpServletResponse.getOutputStream().write(body);
+    }
+}
