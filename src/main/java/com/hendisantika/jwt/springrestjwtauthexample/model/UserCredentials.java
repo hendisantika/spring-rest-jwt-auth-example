0c78512c169af549c9dc9616fd81a131b5f1054a
Hendi Santika <hendisantika@gmail.com>
2019-01-06 16:49:49 +0700

    #4 - Add User Credentials

---
+++ src/main/java/com/hendisantika/jwt/springrestjwtauthexample/model/UserCredentials.java
@@ -0,0 +1,40 @@
+package com.hendisantika.jwt.springrestjwtauthexample.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-rest-jwt-auth-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-06
+ * Time: 16:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserCredentials {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCredentials{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
